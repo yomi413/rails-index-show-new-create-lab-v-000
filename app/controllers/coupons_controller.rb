6ce18f8655cb057658d2bafac7637fbcdd049001
Yomaira Escano <yjescano@gmail.com>
2018-03-07 06:38:50 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -1,3 +1,9 @@
 class CouponsController < ApplicationController
-  
+
+  def create 
+    @coupon = Coupon.new
+    @coupon.coupon_code = params[:coupon_code]
+    @coupon.store = params[:store]
+    @coupon.save
+  end
 end
