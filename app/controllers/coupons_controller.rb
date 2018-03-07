625f42dad3142555e22052cfb10154e360e861dd
Yomaira Escano <yjescano@gmail.com>
2018-03-07 06:58:17 +0000

    automatically backed up by learn

--- app/controllers/coupons_controller.rb
+++ app/controllers/coupons_controller.rb
@@ -4,6 +4,10 @@ class CouponsController < ApplicationController
     @coupons = Coupon.all
   end
 
+  def show
+    @coupon = Coupon.find(params[:id])
+  end
+
   def create
     @coupon = Coupon.new
     @coupon.coupon_code = params[:coupon_code]
